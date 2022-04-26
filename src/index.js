1bcbd845744e6021222d1479417d9b84de2fb9fa
Daria Smirnova <darya.smirn1@yandex.ru>
2022-04-26 13:36:48 +0300

    Выполнила обязательное задание урока №23

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import modal from './modules/modal'
 import scroll from './modules/scroll'
 import validation from './modules/validation'
 import tabs from './modules/tabs'
+import slider from './modules/slider'
 
-timer('21 april 2022')
+
+timer('5 may 2022')
 menu()
 modal()
 scroll()
 validation()
 tabs()
+slider()
---
+++ src/modules/slider.js
@@ -0,0 +1,110 @@
+const slider = () => {
+	const sliderBlock = document.querySelector('.portfolio-content')
+	const slides = document.querySelectorAll('.portfolio-item')
+	let dots
+	const timeInterval = 2000
+
+	let currentSlide = 0
+	let interval
+
+	const addDots = () => {
+		const ul = document.createElement('ul')
+		ul.classList.add('portfolio-dots')
+
+		for (let i = 0; i < slides.length; i++) {
+			const li = document.createElement('li')
+			li.classList.add('dot')
+			if (i === 0) {
+				li.classList.add('dot-active')
+			}
+			ul.append(li)
+		}
+		sliderBlock.append(ul)
+	}
+
+	const getDots = () => {
+		return document.querySelectorAll('.dot')
+	}
+
+	const prevSlide = (elems, index, strClass) => {
+		elems[index].classList.remove(strClass)
+	}
+
+	const nextSlide = (elems, index, strClass) => {
+		elems[index].classList.add(strClass)
+
+	}
+
+	const autoSlide = () => {
+		prevSlide(slides, currentSlide, 'portfolio-item-active')
+		prevSlide(dots, currentSlide, 'dot-active')
+
+		currentSlide++
+		if (currentSlide >= slides.length) {
+			currentSlide = 0
+		}
+		nextSlide(slides, currentSlide, 'portfolio-item-active')
+		nextSlide(dots, currentSlide, 'dot-active')
+	}
+
+	const startSlide = (timer = 1500) => {
+		interval = setInterval(autoSlide, timer)
+
+	}
+
+	const stopSlide = () => {
+		clearInterval(interval)
+	}
+
+	sliderBlock.addEventListener('click', e => {
+		e.preventDefault()
+
+		if (!e.target.matches('.dot, .portfolio-btn')) {
+			return
+		}
+
+		prevSlide(slides, currentSlide, 'portfolio-item-active')
+		prevSlide(dots, currentSlide, 'dot-active')
+
+		if (e.target.matches('#arrow-right')) {
+			currentSlide++
+		} else if (e.target.matches('#arrow-left')) {
+			currentSlide--
+		} else if (e.target.classList.contains('dot')) {
+			dots.forEach((dot, index) => {
+				if (e.target === dot) {
+					currentSlide = index
+				}
+			})
+		}
+
+		if (currentSlide >= slides.length) {
+			currentSlide = 0
+		}
+
+		if (currentSlide < 0) {
+			currentSlide = slides.length - 1
+		}
+
+		nextSlide(slides, currentSlide, 'portfolio-item-active')
+		nextSlide(dots, currentSlide, 'dot-active')
+	})
+
+	sliderBlock.addEventListener('mouseenter', e => {
+		if (e.target.matches('.dot, .portfolio-btn')) {
+			stopSlide(timeInterval)
+		}
+	}, true)
+
+	sliderBlock.addEventListener('mouseleave', e => {
+		if (e.target.matches('.dot, .portfolio-btn')) {
+			startSlide()
+		}
+	}, true)
+
+	addDots()
+	dots = getDots()
+	startSlide(timeInterval)
+}
+
+export default slider
