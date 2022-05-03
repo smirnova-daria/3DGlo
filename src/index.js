d2f3a13caefea3ff63043fa5246a4d3564220b3b
Daria Smirnova <darya.smirn1@yandex.ru>
2022-05-03 17:05:51 +0300

    Выполнила обязательное задание урока №27

--- src/index.js
+++ src/index.js
@@ -2,17 +2,32 @@ import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
 //import scroll from './modules/scroll'
-import validation from './modules/validation'
+//import validation from './modules/validation'
 import tabs from './modules/tabs'
 import slider from './modules/slider'
 import calc from './modules/calc'
+import sendForm from './modules/sendForm'
+
 
 
 timer('5 may 2022')
 menu()
 modal()
 //scroll()
-validation()
+//validation()
 tabs()
 slider('portfolio-content', 'portfolio-item', 'portfolio-item-active', 'dot', 'dot-active', 'portfolio-btn')
 calc(100)
+sendForm({
+	formId: 'form1',
+	someElem: [{
+		type: 'block',
+		id: 'total'
+	}]
+})
+sendForm({
+	formId: 'form2'
+})
+sendForm({
+	formId: 'form3'
+})
