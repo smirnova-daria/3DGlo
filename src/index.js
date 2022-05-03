0507c90d10abd86b1c24be3a1142ad04eed5bf45
Daria Smirnova <darya.smirn1@yandex.ru>
2022-05-03 20:30:19 +0300

    Внесла правки

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
 //import scroll from './modules/scroll'
-//import validation from './modules/validation'
+import validation from './modules/validation'
 import tabs from './modules/tabs'
 import slider from './modules/slider'
 import calc from './modules/calc'
@@ -14,7 +14,7 @@ timer('5 may 2022')
 menu()
 modal()
 //scroll()
-//validation()
+validation()
 tabs()
 slider('portfolio-content', 'portfolio-item', 'portfolio-item-active', 'dot', 'dot-active', 'portfolio-btn')
 calc(100)
