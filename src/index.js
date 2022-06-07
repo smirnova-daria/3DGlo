2d022de1a466af52c13eebc4b884737b762f54ff
Daria Smirnova <darya.smirn1@yandex.ru>
2022-06-07 17:59:54 +0300

    Обновление таймера

--- src/index.js
+++ src/index.js
@@ -1,41 +1,52 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
-//import scroll from './modules/scroll'
+import scroll from './modules/scroll'
 import validation from './modules/validation'
 import tabs from './modules/tabs'
 import slider from './modules/slider'
 import calc from './modules/calc'
 import sendForm from './modules/sendForm'
 
-
-
-timer('20 may 2022')
+timer('20 june 2022')
 menu()
 modal()
-//scroll()
+scroll()
 validation()
 tabs()
-slider('portfolio-content', 'portfolio-item', 'portfolio-item-active', 'dot', 'dot-active', 'portfolio-btn')
+slider(
+    'portfolio-content',
+    'portfolio-item',
+    'portfolio-item-active',
+    'dot',
+    'dot-active',
+    'portfolio-btn'
+)
 calc(100)
 sendForm({
-	formId: 'form1',
-	someElem: [{
-		type: 'block',
-		id: 'total'
-	}]
+    formId: 'form1',
+    someElem: [
+        {
+            type: 'block',
+            id: 'total',
+        },
+    ],
 })
 sendForm({
-	formId: 'form2',
-	someElem: [{
-		type: 'block',
-		id: 'total'
-	}]
+    formId: 'form2',
+    someElem: [
+        {
+            type: 'block',
+            id: 'total',
+        },
+    ],
 })
 sendForm({
-	formId: 'form3',
-	someElem: [{
-		type: 'block',
-		id: 'total'
-	}]
+    formId: 'form3',
+    someElem: [
+        {
+            type: 'block',
+            id: 'total',
+        },
+    ],
 })
